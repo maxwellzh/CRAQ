56b9e02cee1272882344637095d7f9b2f96306da
maxwellzh <maxwellzh@outlook.com>
2019-05-19 14:35:32 +0800

    add files to init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# QQ-chatting-record-search
-a simple tool to search info in chatting record of tencent QQ
+##### 处理QQ聊天记录
+
+检索出聊天记录内某关键词出现次数，及每个发言成员发言中出现该词次数，目前程序中1次发言最多计数1次.
+
+目前仅支持`.txt`文本格式，使用方式
+
+```
+python .\chat_cal.py <resource file> <key word> [output file] 
+```
+
+`<resource file>`指定输入文件位置
+
+`<key word>`指定检索关键词
+
+`[output file]`可选参数，设置处理后输出文件位置
---
+++ chat_cal.py
@@ -0,0 +1,173 @@
+'''
+message type:
+2019-xx-xx xx:xx:xx NAME(QQ_ID)
+'''
+import re
+import operator
+import sys
+
+class member(object):
+    def __init__(self, ID, name):
+        self.name = name
+        self.id = ID
+        self.count = 0
+        '''
+        self.talks = {
+          year:{
+              month:{
+                  day:{
+                      hour:{
+                          minute:{
+                              second:{}
+                                 }
+                           }
+                      }
+                    }
+               }
+        }
+        '''
+        self.talks = {}
+
+    def add_message(self, Time, line):
+        this = self.talks
+        for t in Time[:-1]:
+            if t in this:
+                pass
+            else:
+                this[t] = {}
+            this = this[t]
+        if Time[-1] not in this:
+            this[Time[-1]] = ''
+            
+        #time_dict(self.talks, Time)
+        self.talks[Time[0]][Time[1]][Time[2]
+                                     ][Time[3]][Time[4]][Time[5]] += line
+
+    def new_name(self, name):
+        self.name = name
+
+    def get_talks(self, method='date', date=[], key_word=''):
+        times = 0
+        if method == 'key word':
+            result = ''
+            for message in traversing_dict(self.talks):
+                if key_word in message:
+                    result += message
+                    times += 1
+            return times, result
+        elif method == 'date':
+            this = self.talks
+            result = ''
+            for t in date[:-1]:
+                if t in this:
+                    pass
+                else:
+                    return times, result
+                this = this[t]
+            if date[-1] in this:
+                for message in traversing_dict(this[date[-1]]):
+                    result += message
+                    times += 1
+                return times, result
+            else:
+                return times, result
+
+
+def traversing_dict(Dict):
+    if type(Dict) is not dict:
+        return Dict
+    for _, value in Dict.items():
+        if type(value) is not dict:
+            yield value
+        else:
+            for x in traversing_dict(value):
+                yield x
+
+
+def time_dict(talks, Time):
+    if len(Time) == 0:
+        return
+    if Time[0] in talks:
+        if len(Time) == 1:
+            return
+        time_dict(talks[Time[0]], talks[1:])
+    else:
+        if len(Time) == 1:
+            talks[Time[0]] = ''
+            return
+        talks[Time[0]] = {}
+        time_dict(talks[Time[0]], talks[1:])
+
+
+def get_info(line):
+    info = line.split(' ')
+    if len(info) > 3:
+        info = info[:2] + [info[2] + info[3]]
+    Time = info[0].split('-')+info[1].split(':')
+    Time = [int(s) for s in Time]
+    name = re.search(r'.*(\(|<)', info[2]).group()[:-1]
+    ID = re.search(r'(\(|<).*(\)|>)', info[2]).group()[1:-1]
+    if len(ID) == 0:
+        ID = re.search(r'<.*>', info[2]).group()[1:-1]
+
+    # Time = [year, month, day, hour, minute, second]
+    return ID, name, Time
+
+def main():
+    members = {}
+    member_talking = None
+    count = 0
+    with open(str(sys.argv[1]), 'rt', encoding='utf-8') as chat_record:
+        for line in chat_record:
+            if line[:4] != '2019' or (line[-2] != ')' and line[-2] != '>'):
+                # not a new message
+                if member_talking != None:
+                    member_talking.add_message(Time, line)
+                else:
+                    continue
+            else:
+                # a new message
+                count += 1
+                ID, name, Time = get_info(line)
+                if ID in members:
+                    # an old member
+                    members[ID].new_name(name)
+                else:
+                    # a new member
+                    members[ID] = member(ID, name)
+
+                #members[ID].add_message(Time)
+                members[ID].count += 1
+                member_talking = members[ID]
+
+    key_word = sys.argv[2]
+    count_all = int(0)
+    count_ID = {}
+    for ID in members.keys():
+        count_ID[ID], talks = members[ID].get_talks(method='key word', key_word=key_word)
+        if talks != '':
+            count_all += count_ID[ID]
+
+    print("\n检索消息记录%d条" % (count))
+    for ID in members.keys():
+        print('%-8s\t发消息%d条\t占比%.2f%%' % (members[ID].name, members[ID].count, members[ID].count/count*100))
+    
+    print("\n关键词“%s”出现%d次\n" % (key_word, count_all))
+    for ID in count_ID.keys():
+        if count_ID[ID] != 0:
+            print("%-8s\t发了%d次“%s”\t占比%.2f%%" %
+                (members[ID].name, count_ID[ID], key_word, count_ID[ID]/count_all*100))
+    print('\n')
+
+
+    if len(sys.argv) > 3:
+        with open(sys.argv[3], 'wt', encoding='utf-8') as file_dealed:
+            for ID in members.keys():
+                file_dealed.write('QQ ID/EMAIL: %s\n' % (ID))
+                file_dealed.write('QQ NAME: %s\n' % (members[ID].name))
+                for line in traversing_dict(members[ID].talks):
+                    #print(date)
+                    file_dealed.write(line)          
+
+if __name__ == "__main__":
+    main()

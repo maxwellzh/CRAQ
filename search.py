520b098505d0da18a36c4a55fd80cb147d45aee7
maxwell <maxwellzh@outlook.com>
2019-11-16 00:57:35 +0800

    mv getopt to argparse

--- search.py
+++ search.py
@@ -7,57 +7,51 @@ xxxx-xx-xx xx:xx:xx NAME(ID)/NAME<Email>
 r'^\d{4}-\d{2}-\d{2}\s\d{1,2}:\d{1,2}:\d{1,2}\s.*(\(\d+\)|<.*>)$'
 '''
 import os.path as path
+import argparse
+import datetime
 import modules
 import sys
 import re
-from getopt import getopt
 
 member = modules.member
+VERSION = str(datetime.date.today()).replace('-', '')
+parser = argparse.ArgumentParser(description=("QQ消息文本搜索[%s]" % VERSION), 
+    epilog="获取更详细帮助信息：https://github.com/maxwellzh/CRAQ/blob/master/README.md")
+parser._actions[0].help="显示当前信息."
+parser.add_argument("-i", help="指定输入文件，只添加-i参数时进入菜单模式（推荐）.", 
+    metavar="in-file", type=argparse.FileType('rt'), nargs='+', dest="infile")
+parser.add_argument("-o", help="指定输出文件.", metavar="out-file", 
+    type=argparse.FileType('wt'), dest="outfile")
+search_group = parser.add_mutually_exclusive_group()
+search_group.add_argument("-k", help="指定关键词，不与-r同时使用.",
+    action="store", dest="keyword")
+search_group.add_argument("-r", help="使用正则表达式搜索，不与-k同时使用.",
+    action="store", dest="regular")
+search_group.add_argument("-c", "--count", help="消息统计.", action="store_true",
+    default=False, dest="count")
+parser.add_argument("-v", "--version", help="显示当前程序版本", action="version",
+    version=VERSION)
+
 
 def main():
-    short_opts = ['-h', '-i:', '-o:', '-k:', '-c', '-r:']
-    long_opts = ['help', 'infile', 'outfile', 'kwd', 'count', 'regular']
-    opts, _ = getopt(sys.argv[1:], ''.join(short_opts),
-                     [long_opts[i]+short_opts[i][2:].replace(':', '=') for i in range(len(long_opts))])
+    args = parser.parse_args()
+    # No argument error
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        exit(-1)
+    # Not specify input file but ask for behavior like "-k, -r..."
+    if args.infile==None:
+        print("未指定输入文件.")
+        exit(-1)
 
-    infile = ''
-    outfile = ''
-    word = ''
-    regular = False
-    count_enable = False
-    flag_loop = (len(sys.argv[1:]) == 2 and
-                 (sys.argv[1] == '-i' or sys.argv[1] == '--'+long_opts[1]))
-
-    for op, value in opts:
-        if op in (short_opts[0][:2], '--'+long_opts[0]):    # -h
-            modules.usage()
-            sys.exit()
-        elif op in (short_opts[1][:2], '--'+long_opts[1]):  # -i:
-            infile = str(value)
-            infile = infile.split('+')
-            infile = sorted(infile, key=lambda item: len(item))
-        elif op in (short_opts[2][:2], '--'+long_opts[2]):  # -o:
-            outfile = str(value)
-        elif op in (short_opts[3][:2], '--'+long_opts[3]):  # -k:
-            if word != '':
-                modules.error(3)
-            word = str(value)
-        elif op in (short_opts[4][:2], '--'+long_opts[4]):  # -c
-            count_enable = True
-        elif op in (short_opts[5][:2], '--'+long_opts[5]):  # -r:
-            if word != '':
-                modules.error(3)
-            word = str(value)
-            regular = True
-
-    # deal with 'click and run' on windows explorer 
-    if len(opts) == 0:
-        modules.usage()
-        input('Press Enter to continue')
-        sys.exit()
+    infile = args.infile
+    outfile = args.outfile
+    keyword = args.keyword
+    regular = args.regular
+    count_enable = args.count
 
     members = {}
-    last_talking = None
+    last_msg = None
     count = []
     time_beg = [9999]  # only accurate to day
     time_end = []
@@ -65,19 +59,16 @@ def main():
 
     # read and process input file
     is_new = re.compile(r'^\d{4}-\d{2}-\d{2}\s\d{1,2}:\d{1,2}:\d{1,2}\s.*(\(\d+\)|<.*>)$')
-    for file in infile:
-        if not path.isfile(file):
-            modules.error(4)
-        elif len(file) < 5 or file[-4:] != '.txt':
-            modules.error(5)
+    for f in infile:
+        with f as file:
+            total_lines = modules.get_lines(file)
+            out_interval = int(total_lines/100)
 
-        total_lines = modules.get_lines(file)
-        out_interval = int(total_lines/100)
-
-        with open(file, 'rt', encoding='utf-8') as fin:
             line_cur = int(0)
             out_line = ''
-            for line in fin:
+
+            # First deal with useless head lines
+            for line in file:
                 line_cur += 1
                 if is_new.search(line) == None:
                     continue
@@ -88,9 +79,11 @@ def main():
                         time_beg = Time[:3]
                     if ID not in members:
                         members[ID] = member(ID)
-                    last_talking = members[ID].new_message(name, Time)
+                    last_msg = members[ID].new_message(name, Time)
                     break
-            for line in fin:
+
+            # Deal with main text
+            for line in file:
                 line_cur += 1
                 if is_new.search(line) == None:
                     # not a new message
@@ -99,21 +92,21 @@ def main():
                 else:
                     # a new message
                     count[-1] += 1
-                    last_talking[1] += out_line
+                    last_msg[1] += out_line
 
                     ID, name, Time = modules.get_info(line)
                     if ID not in members:
                         # a new member
                         members[ID] = member(ID)
-                    last_talking = members[ID].new_message(name, Time)
+                    last_msg = members[ID].new_message(name, Time)
                     out_line = ''
 
                 # not accurate
                 if line_cur % out_interval == 0:
                     prop = line_cur/total_lines
-                    print('\rReading %s: %.0f%%|%s%s|' % (file, prop*100,
+                    print('\rReading: %.0f%%|%s%s|' % (prop*100,
                     '#'*int(80*prop), '-'*int(80*(1-prop))), end='')
-            last_talking[1] += out_line
+            last_msg[1] += out_line
         if Time[:3] > time_end:
             time_end = Time[:3]
     print('\r%s' % (' '*200), end='')
@@ -121,21 +114,21 @@ def main():
                                         time_end[0], time_end[1], time_end[2]), end='')
     print('消息记录%d条\n' % (sum(count)))
 
-    # write to output file
-    if outfile != '':
-        modules.out(members, outfile)
 
+    if outfile != None:
+        modules.out(members, outfile)
     if count_enable:
         modules.mysearch(members, '', time_beg, time_end)
+    elif keyword != None:
+        modules.mysearch(members, keyword, time_beg, time_end, regular=False)
+    elif regular != None:
+        modules.mysearch(members, regular, time_beg, time_end, regular=True)
 
-    if word != '':
-        modules.mysearch(members, word, time_beg, time_end, regular=regular)
-
-    # loop menu
-    if flag_loop:
+    # Only -i option, enter MENU mode
+    if not args.count and args.keyword==None and args.outfile==None and args.regular==None:
         modules.menu_usage()
-    while(flag_loop):
-        modules.menu(members, time_beg, time_end)
+        while True:
+            modules.menu(members, time_beg, time_end)
 
 if __name__ == '__main__':
     main()
